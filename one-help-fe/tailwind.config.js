f304c269416a138cf8ef976bc5be1adaf9b61e12
Sofia Prochna <sp.readytogo@gmail.com>
2025-03-12 16:13:13 +0100

    Add Navbar and SideBar components

--- one-help-fe/tailwind.config.js
+++ one-help-fe/tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '2xl': '1.5rem',  // 24px
+        '3xl': '1.875rem', // 30px
+        '4xl': '2.25rem',  // 36px
+        '5xl': '3rem',     // 48px
+        '6xl': '3.75rem',  // 60px
+        '7xl': '4.5rem',   // 72px
+        '8xl': '6rem',     // 96px
+        '9xl': '8rem',     // 128px
+      },
+    },
   },
   plugins: [],
 }
