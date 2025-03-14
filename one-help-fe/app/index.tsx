7422dc1ca92afad858d51a7a231c42d6e245ccc3
Sofia Prochna <sp.readytogo@gmail.com>
2025-03-14 18:21:14 +0100

    Add active raffles Main Screen

--- one-help-fe/app/index.tsx
+++ one-help-fe/app/index.tsx
@@ -2,12 +2,17 @@ import { SafeAreaView, ScrollView } from "react-native";
 import Welcome from "@/components/ui/Welcome";
 import RecentFunsraises from "@/components/ui/RecentFunsraises";
 import RecentEvents from "@/components/ui/RecentEvents";
+import RecentRaffles from "@/components/ui/RecentRaffles";
 
 export default function HomeScreen() {
   return (
     <>
-      <ScrollView>
-        <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView
+        bounces={false} // Disables bounce effect on iOS
+        overScrollMode="never" // Disables over-scroll effect on Android
+        showsVerticalScrollIndicator={false}
+      >
+        <SafeAreaView className="flex-1 bg-white">
           <Welcome
             user={{
               name: "Олеся",
@@ -18,6 +23,7 @@ export default function HomeScreen() {
           />
           <RecentFunsraises />
           <RecentEvents />
+          <RecentRaffles />
         </SafeAreaView>
       </ScrollView>
     </>
